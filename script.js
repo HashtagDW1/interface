32d6fe10a2861232b768b33294b5ba1e8b8801b1
HashtagDW1 <domenic.weinstein@web.de>
2025-04-29 11:17:08 +0200

    pulsebutton

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 document.getElementById('startButton').addEventListener('click', function() {
     window.location.href = 'seite2.html'; // Hier kannst du den Pfad zur nächsten Seite anpassen
 });
+
+
+  const video = document.getElementById("backgroundVideo");
+  const startButton = document.getElementById("startButton");
+
+  video.addEventListener("ended", () => {
+    startButton.classList.add("pulsing");
+  });
+
